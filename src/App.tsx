f0f6f3961b0dee36eca7ad8be70844ee89a79797
Kapza <lukakapetanovic@icloud.com>
2024-04-18 11:33:23 +0200

    Learned about strict mode and what it means

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
-import ListGroup from "./Components/ListGroup";
-import Button from "./Components/Button/Button";
-import "./App.css";
-import Like from "./Components/Like";
+// import ListGroup from "./Components/ListGroup";
+// import Button from "./Components/Button/Button";
+// import "./App.css";
+// import Like from "./Components/Like";
 
-function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// function App() {
+//   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
 
+//   return (
+//     <div>
+//       <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+//       <Button>CLICK ME</Button>
+//       <Like onClick={() => console.log("Clicked")} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import Message from "./Message";
+
+const App = () => {
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
-      {/* <Button>CLICK ME</Button> */}
-      <Like onClick={() => console.log("Clicked")} />
+      <Message />
+
     </div>
   );
-}
+};
 
 export default App;
--- src/message.tsx
+++ src/message.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Message(){
-    const name = 'Luka'
-    if(name)
-        return <h1>Hello {name}</h1>;
-    return <h1>Hello World</h1>
+let count = 0;
 
-}
+const Message = () => {
+  count++;
+  console.log("Message called");
+  return <div>Message {count}</div>;
+};
+
 export default Message;
