1463a3ae9f67dd735ae322ebaa6dcc7ed335b3b9
Kapza <lukakapetanovic@icloud.com>
2024-04-18 12:50:49 +0200

    Added and removed from shopping cart using props.

--- src/App.tsx
+++ src/App.tsx
@@ -22,34 +22,18 @@
 // export default App;
 
 import React, { useState } from "react";
-import Message from "./Message";
-import produce from "immer";
+import Cart from "./Components/Cart";
+import NavBar from "./Components/NavBar";
 
 const App = () => {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
 
-  const handleClick = () => {
-    // setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true}: bug))
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-        console.log(bug);
-        if (bug) bug.fixed = true;
-      })
-    );
-  };
+  const handleClick = () => {};
 
   return (
     <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? "Fixed" : "New"}
-        </p>
-      ))}
-      <button onClick={handleClick}>Click Me</button>
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </div>
   );
 };
