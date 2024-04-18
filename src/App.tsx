f879a865c9be3796f61513c9f882eb2848d36fad
Kapza <lukakapetanovic@icloud.com>
2024-04-18 12:39:39 +0200

    Used Immer to simplify updating state arrays

--- src/App.tsx
+++ src/App.tsx
@@ -23,21 +23,32 @@
 
 import React, { useState } from "react";
 import Message from "./Message";
+import produce from "immer";
 
 const App = () => {
-  const [tags, setTags] = useState(["happy", "cheerful"]);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    //add
-    setTags([...tags, "excited"]);
-    //remove
-    setTags(tags.filter((tag) => tag != "happy"));
-    //update
-    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+    // setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true}: bug))
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        console.log(bug);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
   return (
     <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
