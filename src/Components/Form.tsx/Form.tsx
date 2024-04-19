78477f95a2df8c4d4de0f33e9ac71599bc9b7d9e
Kapza <lukakapetanovic@icloud.com>
2024-04-19 21:33:46 +0200

    Controlled components. Submitting form using state instead of useRef

--- src/Components/Form.tsx/Form.tsx
+++ src/Components/Form.tsx/Form.tsx
@@ -1,14 +1,13 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
 
   const handlesubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
     console.log(person);
   };
   return (
@@ -17,14 +16,26 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          onChange={(e) => setPerson({ ...person, name: e.target.value })}
+          value={person.name} //Now this input field ALWAYS relies on the value from our state variable.Now we are reffering to this field as a controlled component.As its entirely controlled by react.
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
 
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          onChange={(e) => setPerson({ ...person, age: parseInt(e.target.value) })}
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
