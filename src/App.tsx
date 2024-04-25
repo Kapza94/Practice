2f23e8befc95c007173cd8b3a8515c57a01fea48
Kapza <lukakapetanovic@icloud.com>
2024-04-25 15:54:52 +0200

    Adding and removing users with axios.post and axios.delete

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,66 @@ interface User {
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
-
   const [err, setErr] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
-
+    setLoading(true);
     const res = axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", { signal: controller.signal })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setErr(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios.delete("https://jsonplaceholder.typicode.com/users/" + user.id).catch((err) => {
+      setErr(err.message);
+      setUsers(originalUsers);
+    });
+  };
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "Luka" };
+    setUsers([...users, newUser]);
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setErr(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <div>
       {err && <p className="text-danger">{err}</p>}{" "}
-      <ul>
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className="list-group-item d-flex justify-content-between">
+            {user.name}
+            <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
