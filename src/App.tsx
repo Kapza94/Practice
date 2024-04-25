71c3c318ba9fdd75dd3248fe0f33312f00ece04e
Kapza <lukakapetanovic@icloud.com>
2024-04-25 14:14:33 +0200

    Introducing errors from the endpoint and how to handle them.

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,18 @@ interface User {
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
 
+  const [err, setErr] = useState("");
+
   useEffect(() => {
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setErr(err.message));
   }, []);
 
   return (
     <div>
+      {err && <p className="text-danger">{err}</p>}{" "}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
