cfb1122895e41d0c9c9e0ee2739c4c8427f64990
Kapza <lukakapetanovic@icloud.com>
2024-04-25 14:56:23 +0200

    Error management. How to remove and resolve errors and network tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 
 interface User {
   id: number;
@@ -12,18 +12,17 @@ const App = () => {
   const [err, setErr] = useState("");
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        //get returns a promise then gets response obj or error
-        const res = await axios.get<User[]>("https://jsonplaceholder.typicode.com/xusers");
-        setUsers(res.data);
-        // .then((res) => setUsers(res.data))
-        // .catch((err) => setErr(err.message));
-      } catch (err) {
-        setErr((err as AxiosError).message);
-      }
-    };
-    fetchUsers();
+    const controller = new AbortController();
+
+    const res = axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", { signal: controller.signal })
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setErr(err.message);
+      });
+
+    return () => controller.abort();
   }, []);
 
   return (
