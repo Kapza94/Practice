6eab5c07c6fd648a19b04bfd41ff7722f31f9c13
Kapza <lukakapetanovic@icloud.com>
2024-04-25 16:13:10 +0200

    Updating using axios.patch

--- src/App.tsx
+++ src/App.tsx
@@ -53,6 +53,19 @@ const App = () => {
       });
   };
 
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    axios
+      .patch("https://jsonplaceholder.typicode.com/users/" + user.id, updatedUser)
+      .catch((err) => {
+        setErr(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <div>
       {err && <p className="text-danger">{err}</p>}{" "}
@@ -64,9 +77,14 @@ const App = () => {
         {users.map((user) => (
           <li key={user.id} className="list-group-item d-flex justify-content-between">
             {user.name}
-            <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>
-              Delete
-            </button>
+            <div className="d-flex justify-content-between">
+              <button className="btn btn-outline-secondary mx-1" onClick={() => updateUser(user)}>
+                Update
+              </button>
+              <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
